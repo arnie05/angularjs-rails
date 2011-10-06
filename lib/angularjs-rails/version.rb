85a4cad64fde63e57d10048e681cf347d69ab32a
Andrew McElroy <sophrinix@gmail.com>
2011-10-05 19:52:40 -0500

    about to push 0.0.1 to rubygems

--- lib/angularjs-rails/version.rb
+++ lib/angularjs-rails/version.rb
@@ -1,5 +1,9 @@
 module Angularjs
   module Rails
     VERSION = "0.0.1"
+    ANGULAR_VERSION   ="0.9.19"
+    ANGULAR_SCENARIO  ="0.9.19"
+    ANGULAR_MOCKS     ="0.9.19"
+    ANGULAR_IE_COMPAT ="0.9.19"
   end
 end
